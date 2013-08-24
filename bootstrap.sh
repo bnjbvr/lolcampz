abacd38c5f1c5cf0b972b7a4cef8771e57b9b1f5
Benjamin Bouvier <public@benj.me>
2013-08-24 14:28:34 -0700

    bugfix: don't dist-upgrade and install bundler with gem system

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,14 +2,14 @@
 
 # upgrades and installs all dependencies
 apt-get update
-apt-get dist-upgrade -y
-apt-get install -y build-essential ruby1.9.1-full libxml2 libxml2-dev libxslt1-dev postgresql redis-server nodejs git ruby-bundler libpq-dev
+apt-get install -y build-essential ruby1.9.1-full libxml2 libxml2-dev libxslt1-dev postgresql redis-server nodejs git libpq-dev
 
 # Virtualbox guest utils, for automatically mounting /vagrant
 apt-get install -y linux-headers-$(uname -r) virtualbox-guest-utils
 
 # make sure to use ruby1.9.1 as ruby
 update-alternatives --set ruby /usr/bin/ruby1.9.1
+gem install bundler
 
 service postgresql restart
 sudo -u postgres -s psql -f /vagrant/createuser.psql
